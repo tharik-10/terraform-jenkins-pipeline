d6dd58497c9e5b92e65281e9dc51630be0f15949
tharik-10 <md.tharik@mygurukulam.co>
2025-06-17 18:35:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
 variable "ami_id" {
-  default = "ami-020cba7c55df1f615" 
+  default = "ami-05f991c49d264708f" 
 }
 
 variable "instance_type" {
